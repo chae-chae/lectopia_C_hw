32471d542626169c0ac319764679313f08eae5f2
chae-chae <54352086+chae-chae@users.noreply.github.com>
2019-10-12 18:08:15 +0900

    Create hw11.cpp

---
+++ hw11.cpp
@@ -0,0 +1,12 @@
+#pragma warning (disable : 4996)
+#include <stdio.h>
+
+int main(void){
+	int a;
+
+	printf("ASCII code값을 입력하시오 : ");
+	scanf("%d", &a);
+
+	printf("%d은 '%c'의 ASCII code 입니다.\n", a, a);
+	return 0;
+}
